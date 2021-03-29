f2ee6939cd5ff86ca9fcc2c454071c786121b85b
z.skomra <z.skomraa@gmail.com>
2021-03-29 23:58:01 +0200

    feat :: fetching orders from server

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -1,7 +1,32 @@
 import React, { Component } from 'react';
 import Order from '../../components/Order/Order';
+import axios from '../../axios-orders';
+import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
 
 class Orders extends Component {
+    state = {
+        orders: [],
+        loading: true
+    }
+
+    componentDidMount() {
+        axios.get('/orders.json')
+            .then(response => {
+                const fetchOrders = [];
+                for(let key in response.data) {
+                    fetchOrders.push({
+                        ...response.data[key],
+                        id: key
+                    });
+                }
+                console.log(response.data);
+                this.setState({loading: false, orders : fetchOrders});
+            })
+            .catch(error => {
+                this.setState({loading: false});
+            })
+    }
+
         render() {
             return (
                 <div>
@@ -12,4 +37,4 @@ class Orders extends Component {
         }
 }
 
-export default Orders;
+export default withErrorHandler(Orders, axios);
