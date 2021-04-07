d5718e6cefed0b88a813a1f00088fb489722e0c8
z.skomra <z.skomraa@gmail.com>
2021-04-07 22:25:32 +0200

    feat :: dynamically create inputs in form

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -89,13 +89,26 @@ class ContactData extends Component {
     }
 
     render() {
+        const formElementsArray = [];
+        for(let key in this.state.orderForm) {
+            formElementsArray.push({
+                id: key,
+                config: this.state.orderForm[key]
+            });
+        }
+
         let form = (
             <form>
-                    {/* preparing */}
-                    <Input elementType="" elementConfig="" value=""/> 
-                    <Input inputtype="input" type="email" name="email" placeholder="Your email" />
-                    <Input inputtype="input" type="text" name="street" placeholder="Street" />
-                    <Input inputtype="input" type="text" name="postal" placeholder="Postal" />
+                    {formElementsArray.map(formElement =>(
+                        <Input
+                            key={formElement.id} 
+                            elementType={formElement.config.elementType}
+                            elementConfig={formElement.config.elementConfig}
+                            value={formElement.config.value}
+                        />
+                    )
+                        )
+                    }
                     <Button buttonType="Positive" clicked={this.orderHanlder}>ORDER</Button>
                 </form>
         );
