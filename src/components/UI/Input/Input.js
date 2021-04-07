a4ddf8a31c0772364c6b772bb032cc16cea2903a
z.skomra <z.skomraa@gmail.com>
2021-04-07 22:36:30 +0200

    feat :: add select to input component

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -18,6 +18,18 @@ const input = (props) => {
                 value = {props.value}
                 />;
             break;
+        case ( 'select'): 
+            inputElement = <select className={classes.InputElement} 
+                {...props.elementConfig}
+                value = {props.value}
+                >
+                    {props.elementConfig.options.map(option => (
+                        <option key={option.value} value={option.value}>
+                            {option.displayValue}
+                            </option>
+                    )) }
+                </select>;
+            break;
         default: 
             inputElement = <input className={classes.InputElement} 
                 {...props.elementConfig} 
